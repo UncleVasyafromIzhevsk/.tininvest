79fad9f2c0c78b86e6bc5f5408d369898e75ab3e
Babushka Tamarushka <chirkoff3@gmail.com>
2024-01-09 20:31:26 +0400

    90124

--- tinvest.py
+++ tinvest.py
@@ -1,106 +1,198 @@
-from kivy.lang import Builder
+import re
+import numpy as np
+
 from kivymd.app import MDApp
-from kivymd.uix.button import MDFlatButton
-from kivymd.uix.dialog import MDDialog
+from kivy.lang import Builder
+from kivy.uix.screenmanager import (
+    ScreenManager, Screen,
+    FadeTransition, CardTransition)
 from kivymd.uix.card import MDCard
-from kivy.properties import StringProperty
-from kivymd.uix.boxlayout import MDBoxLayout
+from kivymd.uix.label import MDLabel
+from kivy.uix.widget import Widget
+from kivy.graphics import *
+from kivy.properties import (
+    StringProperty, NumericProperty,
+    ListProperty, ObjectProperty,
+    BooleanProperty,
+    BoundedNumericProperty,
+    OptionProperty,
+    ReferenceListProperty,
+    AliasProperty, DictProperty,
+    VariableListProperty,
+    ConfigParserProperty,
+    ColorProperty
+)
 
-from url_request.url_request import UrlRequest
 
-import os
-import sys
+# главный экран
+class MainWindow(Screen):
+    pass
 
-# Обработка ошибок
-from tinkoff.invest import RequestError
 
-# Получение токена из файла
-token = None
-with open('token.txt', 'r') as f:
-            token = f.read()
-            
-# Объект для работы с запросами
-url = UrlRequest(token)
-    
-# id счета(пока одного)
-def id_score():
-    try:
-        a = str((url.url_req_schet()).accounts[0].id)
-        return a
-    except RequestError:
-        a = None
-        return a
-idScore = id_score()
+# второй экран приложения
+class Screen_Info_Tools(Screen):
+    pass
 
-   
-# Диалог ввода токена
-class EnterToken(MDBoxLayout):
-    adaptive_height=True
-        
-        
-# Карты инструментов на экране портфеля
-class CardTools(MDCard):
-    text = StringProperty()
-    
 
-class Tinvest(MDApp):
-    
-    # Настройки портфеля
-    enter_token = None
-    
+# Экран авторизации
+class Authorization_Screen(Screen):
+    pass
+
+
+# Линия графика
+class GridGraph(Widget):
+    name = ObjectProperty(None)
+    x1 = NumericProperty()
+    x2 = NumericProperty()
+    y1 = NumericProperty()
+    y2 = NumericProperty()
+
+    def __init__(self, *arg):
+        # Родительский конструктор
+        Widget.__init__(self)
+        # Свой конструктор
+        self.x1 = arg[0]
+        self.y1 = arg[1]
+        self.x2 = arg[2]
+        self.y2 = arg[3]
+
+
+# Свеча графика
+class CandleGraph(Widget):
+    name = ObjectProperty(None)
+    x1 = NumericProperty()
+    x2 = NumericProperty()
+    x3 = NumericProperty()
+    x4 = NumericProperty()
+    y1 = NumericProperty()
+    y2 = NumericProperty()
+    y3 = NumericProperty()
+    y4 = NumericProperty()
+
+    def __init__(self, *arg):
+        # Родительский конструктор
+        Widget.__init__(self)
+        # Свой конструктор
+        self.x1 = arg[0]
+        self.y1 = arg[1]
+        self.x2 = arg[2]
+        self.y2 = arg[3]
+        self.x3 = arg[4]
+        self.y3 = arg[5]
+        self.x4 = arg[6]
+        self.y4 = arg[7]
+
+
+class TinvestApp(MDApp):
     def build(self):
-        #self.theme_cls.theme_style = "Dark"
-        self.theme_cls.primary_palette = 'Green'
+        sm = ScreenManager(
+            transition=
+            CardTransition()
+        )
+        sm.add_widget(
+            Authorization_Screen(
+                name='login')
+        )
+        sm.add_widget(MainWindow(
+            name='main')
+        )
+        sm.add_widget(
+            Screen_Info_Tools(
+                name=
+                'screen_info_tools')
+        )
+        return sm
 
-        return Builder.load_file ('./kv/tinvest.kv')
-    
-    
-    
-    # Запись токена
-    def token_entry(self, tokken):
-        _token = tokken
-        with open('token.txt', 'w') as f:
-            f.write(_token)
-        url.token = _token
-        
-       
-       
-    # Настройки портфеля
-    def show_confirmation_dialog (self):        
-        if not self.enter_token:
-            self.enter_token = MDDialog (                
-                type="custom",
-                md_bg_color='#2F4F4F',
-                content_cls=EnterToken(),                
+    # Сетка графика эк. инструмента
+    def graf_line(self):
+        # print(type(
+        # self.root.children)
+        # )
+        name_scr = re.split(
+            "'|'", (str(
+                self.root.children)
             )
-        self.enter_token.open ()
-
-
-    # Функция при запуске приложения
-    def on_start(self):
-        try:
-            if token != '':
-                print(url.token)
-                getattr(self.root.ids, 'name_tariff').text = str(url.url_req_tariff().tariff)
-                getattr(self.root.ids, 'value_portfolio').text = (str((
-                    url.url_req_portf(idScore).total_amount_shares).units) + ',' + str((
-                    url.url_req_portf(idScore).total_amount_shares).nano) +
-                                                              ' ' + 'Рублей')
-            else:
-                getattr(self.root.ids, 'name_tariff').text = ('Не указан токен')
-                getattr(self.root.ids, 'value_portfolio').text = ('Не указан токен')      
-        except RequestError:
-            getattr(self.root.ids, 'name_tariff').text = ('Токен не верен или просрочен')
-            getattr(self.root.ids, 'value_portfolio').text = ('Токен не верен или просрочен')
-        
-        for brand in range(1, 20):
-            self.root.ids.inNali_box.add_widget(
-                CardTools(                                        
-                    text=str(brand),                    
-                )
+        )[1]
+        # print(re.split("'|'", (
+        # str(self.root.children)
+        # ))[1])
+        # print(
+        # self.root.get_screen(
+        # re.split("'|'", (str(
+        # self.root.children))
+        # )[1]).ids)
+        plot = self.root.get_screen(
+            name_scr).ids.plot
+        # print(plot.pos)
+        # print(plot.size)
+        a = (plot.size[0] / 9)
+        b = (plot.size[1] / 9)
+        c = 1
+        while c < 9:
+            c += 1
+            plot.add_widget(
+                GridGraph(
+                    a,
+                    plot.pos[1], a,
+                    plot.size[1])
             )
-            
-    
-    
+            plot.add_widget(
+                GridGraph(
+                    plot.pos[0],
+                    b, plot.size[0],
+                    b)
+            )
+            a = (
+                    (plot.size[0] / 9
+                     ) * c
+            )
+            b = (
+                    (plot.size[1] / 9
+                     ) * c
+            )
 
-Tinvest().run()
+    # Очистка экрана
+    # графика экрана инструмента
+    def clear_graf(self):
+        name_scr = re.split(
+            "'|'", (str(
+                self.root.children)
+            )
+        )[1]
+        self.root.get_screen(
+            name_scr).ids.plot.clear_widgets(
+            children=None)
+
+    # Свеча графика
+    def graf_candle(self):
+        name_scr = re.split(
+            "'|'", (str(
+                self.root.children)
+            )
+        )[1]
+        plot = self.root.get_screen(
+            name_scr
+        ).ids.plot
+
+        data = np.array(
+            [[0, plot.size[1]],
+             [69, 67], [120, 110]]
+        )
+        scaled_data = (data -
+                       np.mean(data,
+                               axis=0
+                               )
+                       ) / np.std(
+            data, axis=0
+        )
+        print(scaled_data)
+
+        # print(y1, y2)
+        # plot.add_widget(
+        # CandleGraph((
+        # plot.size[0] / 9), y1,
+        # (plot.size[0] / 9),
+        # y2, 65, 43, 65, 45))
+
+
+TinvestApp().run()
